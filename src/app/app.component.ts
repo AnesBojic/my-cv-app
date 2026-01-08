e8a8a79aa58b4a371853b1e2d7d32cf054acc072
AnesBojic <anes.bojic100@gmail.com>
2026-01-08 23:12:08 +0100

    Created my CV

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+// Importuj svoju komponentu (putanja zavisi gdje si je kreirao)
+import { MyCvComponent } from './my-cv/my-cv.component'; // <-- DODAJ OVO
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+
+  imports: [CommonModule, RouterOutlet, MyCvComponent], // <-- I OVO
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-cv-app';
+  title = 'moj-cv';
 }
